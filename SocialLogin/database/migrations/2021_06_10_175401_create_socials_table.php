34025a31a1cf40d4716857b92d61fc446d1bf5ae
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-06-10 23:57:24 +0600

    Database Table

---
+++ SocialLogin/database/migrations/2021_06_10_175401_create_socials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSocialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('socials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');
+            $table->string('name');
+            $table->string('nick_name');
+            $table->string('email')->unique();
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('socials');
+    }
+}
